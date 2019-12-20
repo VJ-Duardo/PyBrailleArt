d51ceee0178da754eb094d10f5268c2e5ff9f2bc
VJ-Duardo <olafstein501@gmail.com>
2019-12-20 16:10:53 +0100

    convertes single dot back to full character

--- invertbraille.py
+++ invertbraille.py
@@ -12,7 +12,9 @@ braille_dic = {}
 for i in range(0, len(braille_str), 1):
     braille_dic[braille_str[i]] = braille_str[len(braille_str) - (i + 1)]
 
+#you may want to comment these out if you to have ⣿ be replaced with an invisible character instead of dot
 braille_dic['⣿'] = '⠄'
+braille_dic['⠄'] = '⣿'
 
 result_str = ""
 for i in range(0, len(input_str), 1):

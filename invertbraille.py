fe68b49fe225b57e546caae1b720a4f07b40dcf0
VJ-Duardo <olafstein501@gmail.com>
2019-12-27 21:43:50 +0100

    added turn function and improved interface

--- invertbraille.py
+++ invertbraille.py
@@ -1,26 +1,67 @@
 # -*- coding: utf-8 -*-
 
-braille_str = "⠀⠁⠂⠃⠄⠅⠆⠇⠈⠉⠊⠋⠌⠍⠎⠏⠐⠑⠒⠓⠔⠕⠖⠗⠘⠙⠚⠛⠜⠝⠞⠟⠠⠡⠢⠣⠤⠥⠦⠧⠨⠩⠪⠫⠬⠭⠮⠯⠰⠱⠲⠳⠴⠵⠶" \
-              + "⠷⠸⠹⠺⠻⠼⠽⠾⠿⡀⡁⡂⡃⡄⡅⡆⡇⡈⡉⡊⡋⡌⡍⡎⡏⡐⡑⡒⡓⡔⡕⡖⡗⡘⡙⡚⡛⡜⡝⡞⡟⡠⡡⡢⡣⡤⡥⡦⡧⡨⡩⡪⡫⡬" \
-              + "⡭⡮⡯⡰⡱⡲⡳⡴⡵⡶⡷⡸⡹⡺⡻⡼⡽⡾⡿⢀⢁⢂⢃⢄⢅⢆⢇⢈⢉⢊⢋⢌⢍⢎⢏⢐⢑⢒⢓⢔⢕⢖⢗⢘⢙⢚⢛⢜⢝⢞⢟⢠⢡⢢" \
-              + "⢣⢤⢥⢦⢧⢨⢩⢪⢫⢬⢭⢮⢯⢰⢱⢲⢳⢴⢵⢶⢷⢸⢹⢺⢻⢼⢽⢾⢿⣀⣁⣂⣃⣄⣅⣆⣇⣈⣉⣊⣋⣌⣍⣎⣏⣐⣑⣒⣓⣔⣕⣖⣗⣘" \
-              + "⣙⣚⣛⣜⣝⣞⣟⣠⣡⣢⣣⣤⣥⣦⣧⣨⣩⣪⣫⣬⣭⣮⣯⣰⣱⣲⣳⣴⣵⣶⣷⣸⣹⣺⣻⣼⣽⣾⣿"
+braille_str_invert = "⠀⣿⠁⣾⠂⣽⠃⣼⠄⣻⠅⣺⠆⣹⠇⣸⠈⣷⠉⣶⠊⣵⠋⣴⠌⣳⠍⣲⠎⣱⠏⣰⠐⣯⠑⣮⠒⣭⠓⣬⠔⣫⠕⣪⠖⣩⠗⣨⠘⣧" \
+                     "⠙⣦⠚⣥⠛⣤⠜⣣⠝⣢⠞⣡⠟⣠⠠⣟⠡⣞⠢⣝⠣⣜⠤⣛⠥⣚⠦⣙⠧⣘⠨⣗⠩⣖⠪⣕⠫⣔⠬⣓⠭⣒⠮⣑⠯⣐⠰⣏⠱⣎" \
+                     "⠲⣍⠳⣌⠴⣋⠵⣊⠶⣉⠷⣈⠸⣇⠹⣆⠺⣅⠻⣄⠼⣃⠽⣂⠾⣁⠿⣀⡀⢿⡁⢾⡂⢽⡃⢼⡄⢻⡅⢺⡆⢹⡇⢸⡈⢷⡉⢶⡊⢵" \
+                     "⡋⢴⡌⢳⡍⢲⡎⢱⡏⢰⡐⢯⡑⢮⡒⢭⡓⢬⡔⢫⡕⢪⡖⢩⡗⢨⡘⢧⡙⢦⡚⢥⡛⢤⡜⢣⡝⢢⡞⢡⡟⢠⡠⢟⡡⢞⡢⢝⡣⢜" \
+                     "⡤⢛⡥⢚⡦⢙⡧⢘⡨⢗⡩⢖⡪⢕⡫⢔⡬⢓⡭⢒⡮⢑⡯⢐⡰⢏⡱⢎⡲⢍⡳⢌⡴⢋⡵⢊⡶⢉⡷⢈⡸⢇⡹⢆⡺⢅⡻⢄⡼⢃" \
+                     "⡽⢂⡾⢁⡿⢀"
 
-input_str = "⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠉⠉⠉⠉⠋⠉⠉⠙⠛⠛⠻⠿⢿⣿⣿⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⠿⠋⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠉⠻⣿⣿⣿⣿⣿ ⣿⣿⡟⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠙⢻⣿⣿⣿ ⣿⠏⠄⠄⠄⠄⠄⠄⠄⠄⢀⣔⢤⣄⡀⠄⡄⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⢻⣿⣿  ⠏⠄⠄⠄⠄⠄⠄⠄⢀⣀⣨⣵⣿⣿⣿⣿⣧⣦⣤⣀⣿⣷⡐⠄⠄⠄⠄⠄⢿⣿ ⠄⠄⠄⠄⠄⠄⠐⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠄⠄⠄⢚⣿ ⣆⠄⠄⠄⠄⠄⠄⢻⣿⣿⣿⣿⡿⠛⠛⠛⠛⣿⢿⣿⣿⣿⡿⢟⣻⣄⣤⣮⡝⣿  ⣿⠆⠄⠄⠄⠄⠄⠄⠄⠄⠉⠘⣿⡗⡕⣋⢉⣩⣽⣬⣭⣶⣿⣿⣿⣿⣝⣻⣷⣿ ⣿⣦⡀⠄⠄⠠⢀⠄⠄⠁⠄⠄⣿⣿⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣿ ⣿⣿⣿⡆⠄⠄⠰⣶⡗⠄⠄⠄⣿⣿⣿⣿⣦⣌⠙⠿⣿⣿⣿⣿⣿⣿⣿⡛⠱⢿  ⣿⣿⣿⣿⡀⠄⠄⠿⣿⠄⠄⠄⠨⡿⠿⠿⣿⣟⣿⣯⣹⣿⣿⣿⣿⣿⣿⣿⣦⡀ ⣿⣿⣿⣿⣷⠄⠄⠄⢷⣦⠄⠄⠐⢶⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇ ⣿⣿⣿⣿⣿⣧⡄⠄⠄⠉⠄⠄⠄⢉⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠄ ⣿⣿⣿⣿⣿⠟⠋⠄⠄⠄⠄⠄⠄⠈⠛⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠄⠄ ⣿⠿⠛⠉⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⠿⢿⣿⣿⣿⣿⣿⠿⠋⠄⠄⠄⠄ "
+braille_str_180 = "⠁⢀⠂⠠⠃⢠⠄⠐⠅⢐⠆⠰⠇⢰⠈⡀⠉⣀⠊⡠⠋⣠⠌⡐⠍⣐⠎⡰⠏⣰⠑⢄⠒⠤⠓⢤⠔⠔⠕⢔⠖⠴⠗⢴⠘⡄⠙⣄⠚⡤⠛⣤" \
+                  "⠜⡔⠝⣔⠞⡴⠟⣴⠡⢂⠢⠢⠣⢢⠥⢒⠦⠲⠧⢲⠨⡂⠩⣂⠪⡢⠫⣢⠬⡒⠭⣒⠮⡲⠯⣲⠱⢆⠳⢦⠵⢖⠶⠶⠷⢶⠸⡆⠹⣆⠺⡦" \
+                  "⠻⣦⠼⡖⠽⣖⠾⡶⠿⣶⡁⢈⡃⢨⡅⢘⡇⢸⡈⡈⡉⣈⡊⡨⡋⣨⡌⡘⡍⣘⡎⡸⡏⣸⡑⢌⡓⢬⡕⢜⡗⢼⡙⣌⡚⡬⡛⣬⡜⡜⡝⣜" \
+                  "⡞⡼⡟⣼⡡⢊⡣⢪⡥⢚⡧⢺⡩⣊⡪⡪⡫⣪⡭⣚⡮⡺⡯⣺⡱⢎⡳⢮⡵⢞⡷⢾⡹⣎⡻⣮⡽⣞⡾⡾⡿⣾⢁⢁⢃⢡⢅⢑⢇⢱⢉⣁" \
+                  "⢋⣡⢍⣑⢏⣱⢓⢥⢕⢕⢗⢵⢙⣅⢛⣥⢝⣕⢟⣵⢣⢣⢧⢳⢩⣃⢫⣣⢭⣓⢯⣳⢷⢷⢹⣇⢻⣧⢽⣗⢿⣷⣉⣉⣋⣩⣍⣙⣏⣹⣛⣭" \
+                  "⣝⣝⣟⣽⣫⣫⣯⣻⣿⣿"
 
-braille_dic = {}
-for i in range(0, len(braille_str), 1):
-    braille_dic[braille_str[i]] = braille_str[len(braille_str) - (i + 1)]
 
-#you may want to comment these out if you to have ⣿ be replaced with an invisible character instead of dot
-braille_dic['⣿'] = '⠄'
-braille_dic['⠄'] = '⣿'
+def create_braille_dic(braille_str):
+    braille_dic = {}
+    for i in range(0, len(braille_str), 2):
+        braille_dic[braille_str[i]] = braille_str[i + 1]
+        braille_dic[braille_str[i + 1]] = braille_str[i]
 
-result_str = ""
-for i in range(0, len(input_str), 1):
-    try:
-        result_str = result_str + braille_dic[input_str[i]]
-    except KeyError:
-        result_str = result_str + input_str[i]
+    return braille_dic
 
-print(result_str)
+
+def replace_str_chars(input_str, braille_dic):
+    new_str = ""
+    for i in range(0, len(input_str), 1):
+        try:
+            new_str += braille_dic[input_str[i]]
+        except KeyError:
+            new_str += input_str[i]
+
+    return new_str
+
+
+def invert(input_str):
+    braille_dic_invert = create_braille_dic(braille_str_invert)
+
+    # you may want to comment these out if you want to have '⣿' be replaced with an invisible '⠀' character instead of '⠄'
+    braille_dic_invert['⣿'] = '⠄'
+    braille_dic_invert['⠄'] = '⣿'
+
+    result_str = replace_str_chars(input_str, braille_dic_invert)
+
+    return result_str
+
+
+def turn(input_str):
+    braille_dic_180 = create_braille_dic(braille_str_180)
+
+    replaced_str = replace_str_chars(input_str, braille_dic_180)
+
+    line_arr = replaced_str.split(" ")
+    result_str = ""
+    for i in range(len(line_arr) - 1, 0 - 1, -1):
+        reversed_line = line_arr[i][len(line_arr[i])::-1]
+        result_str += reversed_line + " "
+
+    return result_str
+
+
+braille_input_str = """⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠉⠉⠉⠉⠋⠉⠉⠙⠛⠛⠻⠿⢿⣿⣿⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⠿⠋⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠉⠻⣿⣿⣿⣿⣿ ⣿⣿⡟⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠙⢻⣿⣿⣿ ⣿⠏⠄⠄⠄⠄⠄⠄⠄⠄⢀⣔⢤⣄⡀⠄⡄⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⢻⣿⣿  ⠏⠄⠄⠄⠄⠄⠄⠄⢀⣀⣨⣵⣿⣿⣿⣿⣧⣦⣤⣀⣿⣷⡐⠄⠄⠄⠄⠄⢿⣿ ⠄⠄⠄⠄⠄⠄⠐⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠄⠄⠄⢚⣿ ⣆⠄⠄⠄⠄⠄⠄⢻⣿⣿⣿⣿⡿⠛⠛⠛⠛⣿⢿⣿⣿⣿⡿⢟⣻⣄⣤⣮⡝⣿  ⣿⠆⠄⠄⠄⠄⠄⠄⠄⠄⠉⠘⣿⡗⡕⣋⢉⣩⣽⣬⣭⣶⣿⣿⣿⣿⣝⣻⣷⣿ ⣿⣦⡀⠄⠄⠠⢀⠄⠄⠁⠄⠄⣿⣿⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣿ ⣿⣿⣿⡆⠄⠄⠰⣶⡗⠄⠄⠄⣿⣿⣿⣿⣦⣌⠙⠿⣿⣿⣿⣿⣿⣿⣿⡛⠱⢿  ⣿⣿⣿⣿⡀⠄⠄⠿⣿⠄⠄⠄⠨⡿⠿⠿⣿⣟⣿⣯⣹⣿⣿⣿⣿⣿⣿⣿⣦⡀ ⣿⣿⣿⣿⣷⠄⠄⠄⢷⣦⠄⠄⠐⢶⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇ ⣿⣿⣿⣿⣿⣧⡄⠄⠄⠉⠄⠄⠄⢉⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠄ ⣿⣿⣿⣿⣿⠟⠋⠄⠄⠄⠄⠄⠄⠈⠛⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠄⠄ ⣿⠿⠛⠉⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⠿⢿⣿⣿⣿⣿⣿⠿⠋⠄⠄⠄⠄ """
+
+print(invert(braille_input_str))
+print(turn(braille_input_str))
